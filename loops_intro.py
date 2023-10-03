7376128049a189abdbf1ede3df67ce08c4573fc7
Darima Pivovarova <dpivovarova@gmail.com>
2023-10-03 17:30:55 +0200

    loops

---
+++ loops_intro.py
@@ -0,0 +1,14 @@
+# Write a program that shows the first five odd numbers
+for i in [0,2,4,6,8]:
+    a = i + 1
+    print(a)
+
+# Write a program that shows the first five terms of the harmonic sequence
+# Write your code here
+for i in [2,4,6,8,10]:
+    print ("1/", i)
+
+# Write a program that shows the first five terms of the geometric sequence
+# Write your code here
+for i in [0, 1, 2, 3, 4]:
+    print(3**i)
