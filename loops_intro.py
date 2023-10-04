d9eafb8db88eb54542eac73b05b31e094cfffcb6
Darima Pivovarova <dpivovarova@gmail.com>
2023-10-04 15:50:22 +0200

    loops 2

--- loops_intro.py
+++ loops_intro.py
@@ -22,3 +22,25 @@ for i in range(0,200, 2):
 # Write your code here
 for i in range(0,100,5):
     print(i+1)
+
+# Write a program that takes an integer input by the user and prints a multiplication table for the first 20 multiples, as illustrated below.
+# Write you code below
+a = int(input("enter a number "))
+for i in range (1,21):
+    print(a*i)
+
+# Write a program that calculates the average of the numbers that have been input by the user. The program first asks the user how many values they want to average. The number of values should be greater than
+
+count_numbers = int(input("how many numbers you will enter? "))
+print(count_numbers)
+for i in range(1, count_numbers+1):
+    print(input("enter a number "))
+
+# Write a program that shows all the factors of a number that have been provided by the user
+# Write you code below
+input_number = int(input("enter a number: "))
+i = 1
+while i <= input_number:
+    if (input_number % i == 0):
+        print(i)
+    i = i + 1
