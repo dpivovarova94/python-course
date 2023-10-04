731d31d54d9fb654511745becede1f2e1c121cfd
Darima Pivovarova <dpivovarova@gmail.com>
2023-10-04 18:46:03 +0200

    nested loops

--- loop_try.py
+++ loop_try.py
@@ -1,4 +1,12 @@
-print(5*"*")
+# num = 23
+# gum = 1
+# while (num > 0):
+#    gum = num % 10
+#    num //= 10
+#    print(gum)
+#    print(num)
+# print(gum)
 
+# first loop: gum = 3, num = 2
+# second loop: gum = 2 % 10 = 2
 
-# print(3**(6)=1000)
