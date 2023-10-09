f10f880744129e68a273f40ccccbb2777944acfb
Darima Pivovarova <dpivovarova@gmail.com>
2023-10-09 15:47:04 +0200

    functions

---
+++ functions.py
@@ -0,0 +1,8 @@
+# Write your code below
+def showTableOf4():
+    for i in range(1,21):
+        print("4 *", i, end="" )
+        print(" = ", end="" )
+        print(4 * i)
+
+showTableOf4()        
